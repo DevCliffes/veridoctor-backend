6ae76b44257b6628591ecabd6603a01e2dadfcce
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-06 03:02:48 +0300

    Add superuser creation to startup script
    Add command to create a superuser if it doesn't exist

--- startup.sh
+++ startup.sh
@@ -3,4 +3,5 @@ set -e
 echo "Running migrations..."
 python manage.py migrate --no-input
 echo "Starting server..."
+python manage.py createsuperuser --noinput --username admin --email admin@veridoctor.com || true
 gunicorn config.wsgi --bind 0.0.0.0:10000 --workers 1 --timeout 120
