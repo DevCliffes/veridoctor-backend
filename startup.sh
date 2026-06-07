cec12db5dc7c0adf6b76424f516b5b843b4afb27
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-08 02:57:49 +0300

    Add makemigrations command to startup script

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 echo "Running migrations..."
+python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 echo "Starting server..."
 gunicorn config.wsgi --bind 0.0.0.0:10000 --workers 1 --timeout 120
