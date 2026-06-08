2415c35d65860c862cfc291aebf28f8a74094cd1
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-08 23:54:12 +0300

    Import serializers for Service and Form

--- provider/views.py
+++ provider/views.py
@@ -1,3 +1,5 @@
+from .models import HealthcareProvider, Service, Form
+from .serializers import ServiceSerializer, FormSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
