b5e5df12539ba2d5abc850ca991197d1fce7663c
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-08 23:52:06 +0300

    Add FormSerializer for Form model

--- provider/serializers.py
+++ provider/serializers.py
@@ -15,3 +15,8 @@ class ServiceSerializer(serializers.ModelSerializer):
             "created_at",
         ]
         read_only_fields = ["id", "created_at"]
+class FormSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Form
+        fields = ["id", "name", "sections", "created_at", "updated_at"]
+        read_only_fields = ["id", "created_at", "updated_at"]
