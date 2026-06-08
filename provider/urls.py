762639e59e8b3cc4ed2134e85bcdf3254fedbfa0
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-08 03:33:08 +0300

    Add ServiceDetailView URL pattern

--- provider/urls.py
+++ provider/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ServiceView
+from .views import ServiceView, ServiceDetailView
 
 urlpatterns = [
     path("<str:identity_id>/services", ServiceView.as_view()),
+    path("<str:identity_id>/services/<str:service_id>", ServiceDetailView.as_view()),
 ]
