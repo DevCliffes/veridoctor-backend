1d08469e75e460646886a686f0db3a213e9fd4d9
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-08 23:57:12 +0300

    Add FormView URL pattern to urls.py

--- provider/urls.py
+++ provider/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ServiceView, ServiceDetailView
+from .views import ServiceView, ServiceDetailView, FormView
 
 urlpatterns = [
     path("<str:identity_id>/services", ServiceView.as_view()),
     path("<str:identity_id>/services/<str:service_id>", ServiceDetailView.as_view()),
+    path("<str:identity_id>/forms", FormView.as_view()),
 ]
