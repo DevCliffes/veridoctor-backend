bd9cd8afd5c19a478b710feb5cf7cfacd10e7bf1
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-10 11:52:53 +0300

    Delete entrypoint2.sh

--- entrypoint2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run schema updates
-python migrate_schema.py
-
-# Run migrations and collect static assets  
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-exec "$@"
