7e5287fa21a25b0ad3e892d3c117d92d62887471
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-13 16:42:30 +0300

    Update UUIDField to use default UUID generation

--- provider/migrations/0002_service_price_visible.py
+++ provider/migrations/0002_service_price_visible.py
@@ -1,3 +1,5 @@
+import uuid
+import django.db.models.deletion
 from django.db import migrations, models
 
 
@@ -22,7 +24,7 @@ class Migration(migrations.Migration):
                 migrations.CreateModel(
                     name='Prescription',
                     fields=[
-                        ('id', models.UUIDField(primary_key=True, serialize=False, editable=False)),
+                        ('id', models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, serialize=False)),
                         ('patient_id', models.CharField(blank=True, max_length=255)),
                         ('patient_name', models.CharField(blank=True, max_length=255, null=True)),
                         ('patient_email', models.EmailField(blank=True, db_index=True, null=True)),
@@ -30,17 +32,27 @@ class Migration(migrations.Migration):
                         ('notes', models.TextField(blank=True, null=True)),
                         ('created_at', models.DateTimeField(auto_now_add=True)),
                         ('updated_at', models.DateTimeField(auto_now=True)),
+                        ('provider', models.ForeignKey(
+                            on_delete=django.db.models.deletion.CASCADE,
+                            related_name='prescriptions',
+                            to='provider.healthcareprovider',
+                        )),
                     ],
                 ),
                 migrations.CreateModel(
                     name='PrescriptionDrug',
                     fields=[
-                        ('id', models.UUIDField(primary_key=True, serialize=False, editable=False)),
+                        ('id', models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, serialize=False)),
                         ('drug_name', models.CharField(max_length=255)),
                         ('dosage', models.CharField(blank=True, max_length=100, null=True)),
                         ('frequency', models.CharField(max_length=100)),
                         ('duration', models.CharField(max_length=100)),
                         ('instructions', models.TextField(blank=True, null=True)),
+                        ('prescription', models.ForeignKey(
+                            on_delete=django.db.models.deletion.CASCADE,
+                            related_name='drugs',
+                            to='provider.prescription',
+                        )),
                     ],
                 ),
             ],
