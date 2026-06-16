8cb961e19cc3a1052b5b4ed4086fb3edded6802a
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-17 02:22:29 +0300

    Remove unnecessary makemigrations command

--- startup.sh
+++ startup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e
-
 echo "Running schema updates..."
 python migrate_schema.py
-
 echo "Running migrations..."
-python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-
 echo "Starting server..."
 gunicorn config.wsgi --bind 0.0.0.0:10000 --workers 1 --timeout 120
