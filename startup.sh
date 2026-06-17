403e19bc38fe3b5d46314897f1c208834e21eac1
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-18 02:41:02 +0300

    Add backfill command to startup script

--- startup.sh
+++ startup.sh
@@ -4,5 +4,7 @@ echo "Running schema updates..."
 python migrate_schema.py
 echo "Running migrations..."
 python manage.py migrate --no-input
+echo "Backfilling patient record links..."
+python manage.py backfill_patient_records
 echo "Starting server..."
 gunicorn config.wsgi --bind 0.0.0.0:10000 --workers 1 --timeout 120
