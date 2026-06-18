aeadf23ad901e25660414f59c3ba2db5d68e7712
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-19 00:06:06 +0300

    Add PatientTimelineView to URL patterns

--- records/urls.py
+++ records/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import PatientRecordSummaryView
+from .views import PatientRecordSummaryView, PatientTimelineView
 
 urlpatterns = [
     path(
         "patient/<uuid:patient_identity_id>/summary",
         PatientRecordSummaryView.as_view(),
+    ),
+    path(
+        "patient/<uuid:patient_identity_id>/timeline",
+        PatientTimelineView.as_view(),
     ),
 ]
