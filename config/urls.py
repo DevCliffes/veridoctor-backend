73e82267a4296849e1eb088d285249edc011e2e3
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-21 00:47:31 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ urlpatterns = [
     path("provider/", include("provider.urls")),
     path("records/", include("records.urls")),
     path("appointments/", include("appointments.urls")),
-    path("notifications/", include("notifications.urls"))
+    path("notifications/", include("notifications.urls")),
 ]
