e1901c8c1b8277d3892120fae4dff217fbcf7853
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-21 00:42:33 +0300

    Add URL patterns for notification views

---
+++ notifications/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import (
+    NotificationListView,
+    NotificationMarkReadView,
+    NotificationMarkAllReadView,
+)
+
+urlpatterns = [
+    path("", NotificationListView.as_view()),
+    path("mark-all-read", NotificationMarkAllReadView.as_view()),
+    path("<str:notification_id>/read", NotificationMarkReadView.as_view()),
+]
