370f7ad1cdb95769ccde30baaa6fa9bc0f0501d7
DevCliffes <wycliffelichoti94@gmail.com>
2026-06-29 06:04:15 +0300

    Add URL path for sending appointment reminders

--- notifications/urls.py
+++ notifications/urls.py
@@ -3,10 +3,12 @@ from .views import (
     NotificationListView,
     NotificationMarkReadView,
     NotificationMarkAllReadView,
+    SendAppointmentRemindersView,
 )
 
 urlpatterns = [
     path("", NotificationListView.as_view()),
     path("mark-all-read/", NotificationMarkAllReadView.as_view()),
+    path("send-reminders/", SendAppointmentRemindersView.as_view()),
     path("<str:notification_id>/read/", NotificationMarkReadView.as_view()),
 ]
