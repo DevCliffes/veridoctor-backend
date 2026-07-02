0958bec9fe62d315553aa0968baa1293aecb325c
DevCliffes <wycliffelichoti94@gmail.com>
2026-07-02 05:37:55 +0300

    Add push subscription URLs to notifications

--- notifications/urls.py
+++ notifications/urls.py
@@ -5,10 +5,13 @@ from .views import (
     NotificationMarkAllReadView,
     SendAppointmentRemindersView,
 )
+from .push_views import PushSubscribeView, PushUnsubscribeView
 
 urlpatterns = [
     path("", NotificationListView.as_view()),
     path("mark-all-read/", NotificationMarkAllReadView.as_view()),
     path("send-reminders/", SendAppointmentRemindersView.as_view()),
+    path("push-subscribe/", PushSubscribeView.as_view()),
+    path("push-unsubscribe/", PushUnsubscribeView.as_view()),
     path("<str:notification_id>/read/", NotificationMarkReadView.as_view()),
 ]
